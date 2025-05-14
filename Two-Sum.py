300db8ae551da797b91b2941641844ff9259d4b3
SlowJii <lebaohoangth36@gmail.com>
2025-05-15 00:25:39 +0700

    Runtime 1ms, beats 58.7%

---
+++ Two-Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i, num in enumerate(nums):
+            res = target - num
+            if res in hashMap:
+                return [i, hashMap[res]]
+            hashMap[num] = i
