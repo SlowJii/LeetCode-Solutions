575be2cc19cb188c8851a1e0d1c3b5cc69e1a6fc
SlowJii <lebaohoangth36@gmail.com>
2025-05-15 00:51:08 +0700

    Runtime 0ms, Beats 100%

---
+++ Palindrome-Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if (x < 0):
+            return False
+        elif (x >= 0 and x < 10):
+            return True
+        else:
+            return str(x) == str(x)[::-1]
+        
