496fcd28e412e0ae875908646e215b7734bebb47
SlowJii <lebaohoangth36@gmail.com>
2025-08-24 01:06:44 +0700

    add : Complete some Medium SQL practice

---
+++ Database/Medium/Consecutive-Numbers.sql
@@ -0,0 +1,53 @@
+/*
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
+
+WITH ctes AS(
+    SELECT 
+        num,
+        LEAD(num,1) OVER (ORDER BY id) as next_num,
+        LEAD(num,2) OVER (ORDER BY id) as next_next_num
+    FROM Logs
+)
+SELECT DISTINCT num as ConsecutiveNums FROM ctes WHERE num=next_num AND num=next_next_num;
