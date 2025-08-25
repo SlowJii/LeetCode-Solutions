d1766f965073a442f2532eab07cfbdf36907ef30
SlowJii <lebaohoangth36@gmail.com>
2025-08-25 18:53:50 +0700

    add : Runtime 0ms, beats 100%

---
+++ Number-Theory/Easy/Valid-Parentheses.py
@@ -0,0 +1,66 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+"""
+
+# Use stack to solve this problem
+class Solution:
+    def ParenthesesisValid(self, s : str) -> bool:
+        stack = []
+        symbol_dict = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
+        }
+        # {[()]}
+        for char in s:
+            if char in symbol_dict:
+                top_element = stack.pop() if stack else stack.append("#")
+                if symbol_dict[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack 
